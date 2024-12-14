875980b4f7cdff42f4e2a513f1225e5ffff19f85
basilgoodluck <basilgoodluck22@gmail.com>
2024-12-14 19:41:38 +0100

    debugging dashboard data route

--- server/src/routes/dashboardRoute.mjs
+++ server/src/routes/dashboardRoute.mjs
@@ -4,7 +4,13 @@ import dashboardController  from "../controllers/dashboardController.mjs";
 
 const router = Router()
 
-router.get("/users/:userId/dashboard", authenticate, dashboardController)
+router.get("/users/:userId/dashboard", (req, res, next) => {
+    console.log("Request received at:", new Date());
+    console.log("Request params:", req.params);
+    console.log("Authorization header:", req.headers.authorization);
+    next();
+}, authenticate, dashboardController);
+
 
 export default router
 
